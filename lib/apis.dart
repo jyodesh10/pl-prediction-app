5e2540a231c5bde970898afd2306b26a668b7d8d
jyodesh10 <jyodeshshakya@gmail.com>
2024-08-14 18:18:29 +0545

    web

--- lib/apis.dart
+++ lib/apis.dart
@@ -1,16 +1,21 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import "package:http/http.dart" as http;
 
 import 'model.dart';
 
-
+const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 const base = "https://fantasy.premierleague.com/api";
 
 class Apis {
   Future<BootStrapModel> fetchBootstrapData() async {
-    final response = await http.get(Uri.parse('$base/bootstrap-static/'));
-
+    final response = await http.get(Uri.parse('$corsProxy$base/bootstrap-static/'),
+      headers: {
+        'Content-Type': 'application/json', // Add if necessary
+      }
+    );
+    log(response.toString());
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = jsonDecode(response.body);
       return BootStrapModel.fromJson(data);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'local/sharedPref.dart';
@@ -7,12 +8,23 @@ import 'login_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyA4fqUKPIKzyGAeQoZ1btiMz_HaU20X0R8",
-      appId: "1:196895963767:android:e69904550b0a99c7f86472",
-      messagingSenderId: "196895963767",
-      projectId: "pl-prediction-d61a7",
-    ),
+    options:  kIsWeb
+      ? const FirebaseOptions(
+        apiKey: "AIzaSyDJe8-GOLLHm8VqTz31PdmzIq5THpoHcZ0",
+        authDomain: "pl-prediction-d61a7.firebaseapp.com",
+        databaseURL: "https://pl-prediction-d61a7-default-rtdb.firebaseio.com",
+        projectId: "pl-prediction-d61a7",
+        storageBucket: "pl-prediction-d61a7.appspot.com",
+        messagingSenderId: "196895963767",
+        appId: "1:196895963767:web:d827b827435f2922f86472",
+        measurementId: "G-T21ETXLYVX"
+      )
+      : const FirebaseOptions(
+        apiKey: "AIzaSyA4fqUKPIKzyGAeQoZ1btiMz_HaU20X0R8",
+        appId: "1:196895963767:android:e69904550b0a99c7f86472",
+        messagingSenderId: "196895963767",
+        projectId: "pl-prediction-d61a7",
+      ),
   );
   await SharedPrefs().setPreferences();
   runApp(const MyApp());
